cd05488182c263f75c9dc429dc4c120d54847aa5
StevenHsu331 <steven50410@gmail.com>
2023-01-05 23:47:43 +0800

    test

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const app = express();
 app.use(express.json())
 app.use(cors());
 app.use(routes);
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const httpServer = http.createServer(app);
