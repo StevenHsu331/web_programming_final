2b2e3fb85573936f1b9a741b4f964b47408dca35
StevenHsu331 <steven50410@gmail.com>
2023-01-06 00:01:28 +0800

    test

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from "dotenv-defaults";
-import routes from './backend/routes/index.js'
+import routes from './routes/index.js'
 import http from "http";
 import path from "path";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const buildPath = path.join(__dirname, '..', 'build');
 dotenv.config();
 const app = express();
 app.use(express.json())
 app.use(cors());
 app.use(routes);
-app.use(express.static(path.join(__dirname, "public")));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-const httpServer = http.createServer(app);
+app.use(express.static(buildPath));
 
 const port = process.env.PORT || 4000;
-httpServer.listen(port, () => {
+app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
 })
